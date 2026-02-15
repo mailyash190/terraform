1b9cc145331e53892634427478bac1f3d7ab24ae
yash <mailyash190@gmali.com>
2026-02-15 17:56:19 +0530

    new

--- Module/module/main.tf
+++ Module/module/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-    source = "terraform/Module/main/ec2" #to specify the location of the module
+    source = "/home/yashraj/terraform/Module/main/ec2" #to specify the location of the module
     webserver_ami = "ami-019715e0d74f695be"
     webserver_instance_type = "t2.micro"
     webserver_key_name = "jenkins"
@@ -9,7 +9,7 @@ module "ec2" {
 }
 
 module "vpc" {
-    source = "terraform/Module/main/vpc" #to specify the location of the module
+    source = "/home/yashraj/terraform/Module/main/vpc" #to specify the location of the module
     vpc_cidr = "172.30.0.0/16"
     vpc_subnetA = "172.30.1.0/24"
     public_ip = true
